ca8b1260b1105f69e39626697bbc3b0c73284268
Charlie Gleason <hi@superhighfives.com>
2017-01-05 01:33:34 +0000

    Started commenting the playgrounds

--- src/demos/generative.js
+++ src/demos/generative.js
@@ -7,25 +7,30 @@ const playground = () => {
   }
   // sandpit.autoClear = true
   const ctx = sandpit.context
+  ctx.shadowBlur = 5
+  ctx.shadowColor = `rgba(255, 0, 0, 0.5)`
+  ctx.strokeStyle = `rgba(255, 0, 0, 0.1)`
 
+  // Grab a random x, y and corner of the canvas
   const x = Math.random() * sandpit.width
   const y = Math.random() * sandpit.height
   const corner = Mathematics.randomFrom([[0, 0], [sandpit.width, 0], [sandpit.width, sandpit.height], [0, sandpit.height]])
 
   sandpit.loop = () => {
+    // The grid represents the total number of lines to be illustrated
     const grid = sandpit.height / 500
     Array.from({length: grid}).forEach((v, i) => {
+      // There is some randomness here,
+      // to give different illustrations each time
       ctx.beginPath()
       ctx.moveTo(corner[0], corner[1])
-      ctx.shadowBlur = 5
-      ctx.shadowColor = `rgba(255, 0, 0, 0.5)`
-      ctx.strokeStyle = `rgba(255, 0, 0, 0.1)`
       ctx.quadraticCurveTo(x, y - sandpit.time, sandpit.width - sandpit.time, sandpit.height / grid * i + (Math.sin((sandpit.time + (i * 5)) / 1000) * 5000))
       ctx.bezierCurveTo(0, 0, sandpit.width - sandpit.time, sandpit.height / 2, corner[0], corner[1] - sandpit.time)
       ctx.stroke()
     })
   }
 
+  // Let's do this!
   sandpit.start()
 
   // Keep the demo in the query string when resetting
--- src/demos/multitouch.js
+++ src/demos/multitouch.js
@@ -7,10 +7,13 @@
 import Sandpit, { Mathematics } from '../Sandpit'
 
 const playground = () => {
+  // Sandpit supports using an existing canvas - just pass
+  // the reference directly to the constructor
   const existingCanvas = document.createElement('canvas')
   const root = document.querySelector('#root')
   root.appendChild(existingCanvas)
 
+  // Initialise the sandpit and set the settings
   const sandpit = new Sandpit(existingCanvas, Sandpit.CANVAS)
   sandpit.settings = {
     demo: {value: 'multitouch', editable: false, sticky: true},
@@ -21,15 +24,23 @@ const playground = () => {
     decay: {value: 0.92, min: 0.85, max: 0.99, step: 0.01},
     blend: {value: ['darken', 'multiply', 'overlay']}
   }
+
+  // As soon as settings are set, they are accessible in their
+  // simplified form - this will return true, rather than {value: true}
   sandpit.autoClear = sandpit.settings.autoClear
+  // Grab a seeded random, for consistent random results
   const random = sandpit.random()
   const ctx = sandpit.context
 
+  // Define some colours
   const colors = ['hsl(0, 100%, 100%)', 'hsl(175, 100%, 45%)', 'hsl(185, 69%, 63%)', 'hsl(39, 100%, 54%)', 'hsl(333, 100%, 68%)', 'hsl(84, 100%, 68%)', 'hsl(270, 100%, 80%)']
-  let enabled = false
 
+  // The particle definition, including initialisation,
+  // movement, and drawing
   function Particle () {
     this.init = (x, y, radius) => {
+      // Define default values, using the x, y and radius
+      // if they are available
       this.alive = true
 
       this.radius = radius || 10
@@ -46,6 +57,8 @@ const playground = () => {
     }
 
     this.move = () => {
+      // Make the particle move based on its previous
+      // position, drag, and size
       this.x += this.vx
       this.y += this.vy
 
@@ -61,6 +74,7 @@ const playground = () => {
     }
 
     this.draw = () => {
+      // Draw it on the canvas
       ctx.beginPath()
       ctx.arc(this.x, this.y, this.radius, 0, Mathematics.TWO_PI)
       ctx.fillStyle = this.color
@@ -68,19 +82,25 @@ const playground = () => {
     }
   }
 
+  // Set up the particles and pool arrays, for holding existing
+  // particles and reusing ones that are no longer alive
   let particles = []
   let pool = []
 
   const spawn = (x, y, pressure = 0) => {
+    // If there are too many particles, put them into the
+    // pool to be reused
     if (particles.length >= 280) {
       pool.push(particles.shift())
     }
 
+    // Set up the particle
     let particle = pool.length ? pool.pop() : new Particle()
     let size = Mathematics.randomBetween(2, sandpit.settings.maxSize)
     size *= pressure * 2 + 1
     particle.init(x, y, size)
 
+    // Define its characteristics
     particle.wander = Mathematics.randomBetween(0.5, 2.0)
     particle.color = Mathematics.randomFrom(colors)
     particle.drag = Mathematics.randomBetween(sandpit.settings.energy, 0.99)
@@ -91,10 +111,12 @@ const playground = () => {
     particle.vx = Math.sin(theta) * force
     particle.vy = Math.cos(theta) * force
 
+    // Push it into the particle array
     particles.push(particle)
   }
 
   sandpit.setup = () => {
+    // Put 100 particles on the canvas to start with, in a circle
     for (let i = 0; i < 100; i++) {
       let x = (sandpit.width / 2) + Math.sin(i / Math.PI / 5) * 100
       let y = (sandpit.height / 2) + Math.cos(i / Math.PI / 5) * 100
@@ -102,14 +124,15 @@ const playground = () => {
         spawn(x, y)
       }, i * 10)
     }
-    window.setTimeout(() => {
-      enabled = true
-    }, 500)
   }
 
   sandpit.loop = () => {
+    // These can be stored on the loop, or you could check for each
+    // setting individually in change() and handle them differently
     ctx.globalCompositeOperation = sandpit.settings.blend
     sandpit.autoClear = sandpit.settings.autoClear
+    // For each particle that is alive, move it, then draw it,
+    // otherwise move it into the pool to be reused
     for (let i = particles.length - 1; i >= 0; i--) {
       let particle = particles[i]
       if (particle.alive) {
@@ -122,21 +145,25 @@ const playground = () => {
   }
 
   sandpit.change = (setting) => {
+    // If the setting that is changed is 'blend', clear
+    // the canvas
     if (setting === 'blend') sandpit.clear()
   }
 
   sandpit.move = () => {
-    if (enabled) {
-      for (let i = 0; i < sandpit.input.touches.length; i++) {
-        let touch = sandpit.input.touches[i]
-        let max = Mathematics.randomBetween(1, 4)
-        for (let j = 0; j < max; j++) {
-          spawn(touch.x, touch.y, touch.force)
-        }
+    // When the mousemove or touchmove events are fired, spawn
+    // new particles - supporting multitouch and the mouse through
+    // the same input.touches object
+    for (let i = 0; i < sandpit.input.touches.length; i++) {
+      let touch = sandpit.input.touches[i]
+      let max = Mathematics.randomBetween(1, 4)
+      for (let j = 0; j < max; j++) {
+        spawn(touch.x, touch.y, touch.force)
       }
     }
   }
 
+  // Let the party begin
   sandpit.start()
 
   // Keep the demo in the query string when resetting
